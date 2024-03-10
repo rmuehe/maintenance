ad87df69f1e620ceaeaad9b97b281ae019b0d6c4
Ryan Muehe <rmuehe@gmail.com>
2024-03-09 17:29:11 -0800

    Initial commit

---
+++ src/main/java/com/app/maintenance/MaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.app.maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceApplication.class, args);
+    }
+
+}
